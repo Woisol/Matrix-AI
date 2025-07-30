88975364945bc078c47483494bd0fc3627fc3ccd
Woisol-G <Woisol-G@outlook.com>
2025-07-30 10:54:40 +0800

    :bug: fix[frontend]: 修复 swiper 导致的布局问题，完善部分样式，添加 banner

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -5,6 +5,8 @@ import { NzImageModule } from "ng-zorro-antd/image";
 import Swiper from "swiper";
 import { Navigation, Pagination } from "swiper/modules";
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 interface bannerContent {
   img: string;
@@ -19,12 +21,7 @@ interface bannerContent {
   template: `
   <div class="home-con">
     <section class="col col-left">
-      <div id="swiper-con">
-      <!-- If we need pagination -->
-      <div class="swiper-pagination"></div>
-      <!-- If we need navigation buttons -->
-      <div class="swiper-button-prev"></div>
-      <div class="swiper-button-next"></div>
+      <div class="swiper swiper-con">
         <div class="swiper-wrapper">
           @for (content of carouselContents(); track $index) {
             <div class="swiper-slide">
@@ -34,6 +31,11 @@ interface bannerContent {
             </div>
           }
         </div>
+        <!-- If we need pagination -->
+        <div class="swiper-pagination"></div>
+        <!-- If we need navigation buttons -->
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
       </div>
       <table class=""></table>
     </section>
@@ -50,8 +52,11 @@ interface bannerContent {
   `,
   styles: [`
   .home-con{
+    width: 100%;
+    max-width: 100vw;
     display: flex;
     gap: 40px;
+
     &>.col{
       &.col-left{
         flex:1;
@@ -63,41 +68,44 @@ interface bannerContent {
         flex-shrink: 0;
 
       }
-  }
-}
-  #swiper-con{
-    width: 100%;
-    height: min(fit-content,200px);
-    /*height: fit-content;
-    max-height: 200px;*/
-    &>.swiper-wrapper{
-      /*[nz-carousel-content] {*/
-      &>.swiper-slide {
-        overflow: hidden;
-        background: linear-gradient(135deg, #eee, #ccc);
-        border-radius: var(--size-radius);
-      }
     }
   }
-
-    `],
+  .swiper{
+    width: 100%;
+    /* max-width: 100%;*/
+    /*height: min(fit-content,200px);*/
+    height: fit-content;
+    max-height: 200px;
+    overflow: hidden;
+    border-radius: var(--size-radius);
+    background: linear-gradient(135deg, #eee, #ccc);
+    /* 关键：图片元素有可能突破布局() 解决关键就在于设置图片的宽高() */
+    & img{
+      width: 100%;
+      height: 100%;
+    }
+  }
+  `],
 })
 export class HomeComponent implements AfterViewInit {
   // constructor() {
   swiper: Swiper | undefined;
   ngAfterViewInit() {
-    this.swiper = new Swiper('#swiper-con', {
+    //~~ md 这个 .swiper 改了没用的啊？？？
+    this.swiper = new Swiper('.swiper', {
+      // 不设置具体 px 这里要加个 0 否则会导致元素莫名其妙被加个 3.35544e07 px……
+      width: 0,
       loop: true,
       modules: [Navigation, Pagination],
       // ……@note — By changing classes you will also need to change Swiper's CSS to reflect changed classes
       // slideClass: 'swiper-item',
-      pagination: {
-        el: '.swiper-pagination',
+      autoplay: {
+        delay: 1000,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,
       },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
+      effect: 'slide',
+      spaceBetween: 10,
     })
   }
   protected readonly carouselContents = signal<bannerContent[]>([
@@ -106,12 +114,9 @@ export class HomeComponent implements AfterViewInit {
       alt: "2024 年度报告报告",
     },
     {
-      img: "banner-report-2024.png",
-      alt: "2024 年度报告报告",
+      img: "banner-recruit-2025.png",
+      alt: "2025 Matrix 招新",
     },
-    {
-      img: "banner-report-2024.png",
-      alt: "2024 年度报告报告",
-    },
+
   ])
 }
