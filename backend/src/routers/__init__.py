471221a93483423f002ccdff32949f0dc80a4816
RunningKuma <double_kuma@outlook.com>
2025-07-31 16:15:47 +0800

    :sparkles: feat[backend]: construct structure & add README

---
+++ backend/src/routers/__init__.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+# Here you can define your routes
+# Example:
+# @router.get("/example")
+# async def example_route():
+#     return {"message": "This is an example route."}
