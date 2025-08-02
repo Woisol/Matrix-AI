f5ec6e27b26eeab973b027dc19a8b7c5d2a0ee0e
Woisol-G <Woisol-G@outlook.com>
2025-08-02 14:47:36 +0800

    :sparkles: feat: WIP 尝试使用 swiper web-component，无法添加 pagination 与 navigation

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, signal } from "@angular/core";
+import { AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA, signal } from "@angular/core";
 import { RouterLink } from "@angular/router";
 import { NzCarouselModule } from "ng-zorro-antd/carousel";
 import { NzImageModule } from "ng-zorro-antd/image";
@@ -7,36 +7,45 @@ import { Navigation, Pagination } from "swiper/modules";
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import { register } from "swiper/element";
+import { NgModel } from "@angular/forms";
 
 interface bannerContent {
   img: string;
   alt?: string;
   link?: string;
 }
-
+register();
 @Component({
   selector: "app-home",
   imports: [RouterLink, NzImageModule],
   standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   template: `
   <div class="home-con">
     <section class="col col-left">
-      <div class="swiper swiper-con">
-        <div class="swiper-wrapper">
+      <!-- <swiper-container class="swiper-con" loop="true" autoplay="{disableOnInteraction: false,pauseOnMouseEnter: true}" effect="slide" space-between="10" width ="0"> -->
+        <!-- <div class="swiper-wrapper">  navigation="true" pagination="true" -->
+      <swiper-container class="swiper-con"
+        slides-per-view="3"
+        space-between="spaceBetween"
+        centered-slides="true"
+        [pagination]="{ hideOnClick: true }"
+        [breakpoints]="{
+          '768': {
+            slidesPerView: 3,
+          },
+        }">
           @for (content of carouselContents(); track $index) {
-            <div class="swiper-slide">
-              <img nz-image nzSrc="{{content.img}}" [alt]="content.alt" routerLink="{{content.link}}" nzDisablePreview />
+            <swiper-slide class="swiper-slide">
+              <img nz-image nzSrc="{{content.img}}" [alt]="content.alt" routerLink="{{content.link}}" nzDisablePreview loading="lazy" />
               <!-- {{content}} -->
               <!-- <img [src]="content.image" [alt]="content.title" /> -->
-            </div>
+            </swiper-slide>
           }
-        </div>
-        <!-- If we need pagination -->
-        <div class="swiper-pagination"></div>
-        <!-- If we need navigation buttons -->
-        <div class="swiper-button-prev"></div>
-        <div class="swiper-button-next"></div>
-      </div>
+
+        <!-- </div> -->
+      </swiper-container>
       <table class=""></table>
     </section>
     <section class="col col-right">
@@ -70,7 +79,7 @@ interface bannerContent {
       }
     }
   }
-  .swiper{
+  .swiper-con{
     width: 100%;
     /* max-width: 100%;*/
     /*height: min(fit-content,200px);*/
@@ -87,27 +96,27 @@ interface bannerContent {
   }
   `],
 })
-export class HomeComponent implements AfterViewInit {
+export class HomeComponent {
   // constructor() {
-  swiper: Swiper | undefined;
-  ngAfterViewInit() {
-    //~~ md 这个 .swiper 改了没用的啊？？？
-    this.swiper = new Swiper('.swiper', {
-      // 不设置具体 px 这里要加个 0 否则会导致元素莫名其妙被加个 3.35544e07 px……
-      width: 0,
-      loop: true,
-      modules: [Navigation, Pagination],
-      // ……@note — By changing classes you will also need to change Swiper's CSS to reflect changed classes
-      // slideClass: 'swiper-item',
-      autoplay: {
-        delay: 1000,
-        disableOnInteraction: false,
-        pauseOnMouseEnter: true,
-      },
-      effect: 'slide',
-      spaceBetween: 10,
-    })
-  }
+  // swiper: Swiper | undefined;
+  // ngAfterViewInit() {
+  //   //~~ md 这个 .swiper 改了没用的啊？？？
+  //   this.swiper = new Swiper('.swiper', {
+  //     // 不设置具体 px 这里要加个 0 否则会导致元素莫名其妙被加个 3.35544e07 px……
+  //     width: 0,
+  //     loop: true,
+  //     modules: [Navigation, Pagination],
+  //     // ……@note — By changing classes you will also need to change Swiper's CSS to reflect changed classes
+  //     // slideClass: 'swiper-item',
+  //     autoplay: {
+  //       delay: 1000,
+  //       disableOnInteraction: false,
+  //       pauseOnMouseEnter: true,
+  //     },
+  //     effect: 'slide',
+  //     spaceBetween: 10,
+  //   })
+  // }
   protected readonly carouselContents = signal<bannerContent[]>([
     {
       img: "banner-report-2024.png",
