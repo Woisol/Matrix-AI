8be402b6eb9fa319271fd8696eeb5e2a2e745c5e
Woisol-G <Woisol-G@outlook.com>
2025-08-06 15:46:21 +0800

    :sparkles: feat[frontend]: 完善空提交样式

--- frontend/src/app/api/test/assig.ts
+++ frontend/src/app/api/test/assig.ts
@@ -12,30 +12,6 @@ export const testAssigData: AssigData = {
   通过给定的数组构建最大二叉树，并且输出这个树的根节点。
   > **注意**：本题的输入数组保证不为空，且所有元素互不相同。
   `,
-  submit: {
-    time: new Date('2025-08-05T20:40:00'),
-    submitCode: `function constructMaximumBinaryTree(nums: number[]): TreeNode | null {
-      if (nums.length === 0) return null;
-
-      // 找到数组中的最大值及其索引
-      let maxIndex = 0;
-      for (let i = 1; i < nums.length; i++) {
-          if (nums[i] > nums[maxIndex]) {
-              maxIndex = i;
-          }
-      }
-
-      // 创建根节点
-      const root = new TreeNode(nums[maxIndex]);
-
-      // 递归构建左子树和右子树
-      root.left = constructMaximumBinaryTree(nums.slice(0, maxIndex));
-      root.right = constructMaximumBinaryTree(nums.slice(maxIndex + 1));
-
-      return root;
-  }`,
-    score: 60
-  },
   analysis: `# 题解
   \`\`\`c
   function constructMaximumBinaryTree(nums: number[]) {
