1ecb1ab06c2d8691bef6b51e94133d1b92973d8a
Woisol-G <Woisol-G@outlook.com>
2025-08-07 22:19:04 +0800

    :sparkles: feat[frontend]: 初步实现分析 tab，完善 assig 类型，优化部分样式

--- frontend/src/app/api/type/assigment.d.ts
+++ frontend/src/app/api/type/assigment.d.ts
@@ -1,14 +1,17 @@
+import { MatrixAnalysisProps } from "../../pages/course/components/matrix-analyse.component"
 import { AssigId } from "./general"
 
 export type MdContent = string
-export type CodeContent = string
+//! MdCodeContent 需要手动加上 ```[语言类型]...```
+export type MdCodeContent = string
+// export type CodeContent = string
 
 export type SubmitScoreStatus = 'not-submitted' | 'not-passed' | 'passed' | 'full-score'
 
 export type TestSample = {
-  input: CodeContent
-  realOutput: CodeContent
-  expectOutput?: CodeContent
+  input: MdCodeContent
+  realOutput: MdCodeContent
+  expectOutput?: MdCodeContent
 }
 
 export type Submit = {
@@ -18,6 +21,11 @@ export type Submit = {
   submitCode: string
 }
 
+export type Complexity = {
+  time: string
+  space: string
+}
+
 export type AssigData = {
   assigId: AssigId
   title: string
@@ -25,5 +33,14 @@ export type AssigData = {
 
   submit?: Submit
 
-  analysis?: MdContent | null
+  analysis?: {
+    basic: {
+      resolution?: MatrixAnalysisProps
+      knowledgeAnalysis?: MatrixAnalysisProps
+    }
+    aiGen?: {
+      codeAnalysis?: MatrixAnalysisProps
+      learningSuggestions?: MatrixAnalysisProps
+    }
+  }
 }
