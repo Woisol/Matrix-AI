7f1268ac39095d88c8f91aa915bb4484011be726
Woisol-G <Woisol-G@outlook.com>
2025-08-09 22:58:06 +0800

    :sparkles: feat[frontend]: 实现测试运行面板，优化通用按钮样式

--- frontend/src/app/pages/course/components/code-editor.component.ts
+++ frontend/src/app/pages/course/components/code-editor.component.ts
@@ -1,27 +1,55 @@
-import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
 import * as monaco from 'monaco-editor';
+import { NzSplitterComponent, NzSplitterModule } from "ng-zorro-antd/splitter";
+import { NzSplitterPanelComponent } from "../../../../../node_modules/.pnpm/ng-zorro-antd@20.1.2_fc4ae3c25fffe825e77a0aa6abb687ba/node_modules/ng-zorro-antd/splitter/index";
+import { NzFloatButtonModule } from 'ng-zorro-antd/float-button';
+import { NzButtonModule } from "ng-zorro-antd/button";
+import { NzInputModule } from 'ng-zorro-antd/input';
 
 export type EditorLanguage = 'javascript' | 'typescript' | 'c' | 'cpp' | 'json' | 'markdown' | 'python';
 
 @Component({
   selector: 'code-editor',
   standalone: true,
-  imports: [MonacoEditorModule, FormsModule],
+  imports: [MonacoEditorModule, FormsModule, NzSplitterModule, NzFloatButtonModule, NzButtonModule, NzInputModule],
   template: `
     <div class="editor-wrapper">
-      <ngx-monaco-editor
-        [(ngModel)]="code"
-        [options]="editorOptions"
-        (onInit)="onEditorInit($event)"
-        (ngModelChange)="codeChange.emit($event)"
-        class="monaco-editor"
-      ></ngx-monaco-editor>
+      <nz-splitter nzLayout="vertical" class="editor-splitter">
+        <nz-splitter-panel>
+          <ngx-monaco-editor
+          [(ngModel)]="code"
+          [options]="editorOptions"
+          (onInit)="onEditorInit($event)"
+          (ngModelChange)="codeChange.emit($event)"
+          class="monaco-editor"
+          ></ngx-monaco-editor>
+        </nz-splitter-panel>
+        @if(testPanelOpen()) {
+          <nz-splitter-panel nzMin="100" nzDefaultSize="300">
+             <!-- class="hidable" [class.hide]="!testPanelOpen()" -->
+            <div class="test-run-panel">
+              <div class="col left">
+                <textarea nz-input [(ngModel)]="testPanelInput"></textarea>
+              </div>
+              <div class="col right">
+                {{testPanelOutput()}}
+              </div>
+            </div>
+          </nz-splitter-panel>
+          }
+      </nz-splitter>
+      <div class="action-bar">
+        <button class="secondary" (click)="handleTestPanelToggle()">测试面板</button>
+        <button class="secondary hidable" [class.hide]="!testPanelOpen()" (click)="onTestRequest()">运行</button>
+        <button (click)="onSubmitRequest()">提交</button>
+      </div>
     </div>
   `,
   styles: [`
     :host {
+      --size-action-bar: 40px;
 
       width: 100%;
       height: 100%;
@@ -34,9 +62,57 @@ export type EditorLanguage = 'javascript' | 'typescript' | 'c' | 'cpp' | 'json' 
       border-radius: var(--size-radius-sm);
       overflow: hidden;
     }
+    .editor-splitter {
+      height: calc(100% - var(--size-action-bar));
+    }
     .monaco-editor {
       width: 100%;
       height: 100%;
+    }
+    .action-bar{
+      width: 100%;
+      height: var(--size-action-bar);
+      display: flex;
+      gap: 4px;
+      justify-content: flex-end;
+    }
+    .test-run-panel{
+      width: 100%;
+      height: 100%;
+      display: flex;
+      gap: 5px;
+      &>.col{
+        /*display: inline-block;
+        width: 49%;*/
+        height: 100%;
+        flex:1;
+        &>::ng-deep textarea{
+          resize: none;
+          height: 100%;
+        }
+        &.right{
+          padding: 4px 11px;
+          border: 1px solid var(--color-border);
+        }
+      }
+    }
+    .hidable{
+      width: calc-size(auto);
+      height: calc-size(auto);
+      overflow: hidden;
+      transition: width 0.3s ease-in-out,height 0.3s ease-in-out,
+      padding-left 0.3s ease-in-out,padding-right 0.3s ease-in-out,
+        filter 0.3s ease-in-out,
+        box-shadow 0.3s ease-in-out;
+      &.hide{
+        width: 0;
+        height: 0;
+        padding: 0;
+        font-size: 0;
+        border: none;
+        background: none;
+        white-space: nowrap;
+      }
     }
   `]
 })
@@ -52,6 +128,10 @@ export class CodeEditorComponent implements OnChanges {
 
   @Output() codeChange = new EventEmitter<string>();
   @Output() editorReady = new EventEmitter<monaco.editor.IStandaloneCodeEditor>();
+
+  testPanelOpen = signal(false);
+  testPanelInput = signal('');
+  testPanelOutput = signal('等待测试运行……');
 
   // 缓存 options，避免每次变更检测创建新引用触发编辑器重建
   editorOptions: monaco.editor.IStandaloneEditorConstructionOptions = this.buildOptions();
@@ -92,5 +172,16 @@ export class CodeEditorComponent implements OnChanges {
         monaco.languages.register({ id: l.id, extensions: l.extensions });
       }
     });
+  }
+
+  handleTestPanelToggle() {
+    this.testPanelOpen.set(!this.testPanelOpen());
+  }
+
+  onTestRequest() {
+
+  }
+  onSubmitRequest() {
+    // Handle submit request
   }
 }
