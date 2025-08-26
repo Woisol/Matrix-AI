b9c02a5d221f9f8692103eb1292baf33f98277f3
Woisol-G <Woisol-G@outlook.com>
2025-08-26 18:14:31 +0800

    feat[backend]: implement course schemas, change name to fix best practice

--- backend/app/schemas/assignment.py
+++ backend/app/schemas/assignment.py
@@ -20,10 +20,6 @@ class CodeFileInfo(BaseModel):
     filename: str = Field(..., description="代码文件名")
     content: CodeContent = Field(..., description="代码内容")
 
-class CodeSet(BaseModel):
-    filename: str = Field(..., description="文件名")
-    content: CodeContent = Field(..., description="文件内容")
-
 class Complexity(BaseModel):
     time:str = Field(..., description="时间复杂度")
     space:str = Field(..., description="空间复杂度")
@@ -35,7 +31,7 @@ class MatrixAnalysisContent(BaseModel):
 class MatrixAnalysisProps(BaseModel):
     content: list[MatrixAnalysisContent] = Field(..., description="分析内容参数")
     summary: Optional[MdContent]
-    showInEditor: Optional[bool] = Field(None, description="「辑器中显示」按钮")
+    show_in_editor: Optional[bool] = Field(None, description="「辑器中显示」按钮")
 
 class SubmitScoreStatus(str, Enum):
     NOT_SUBMITTED = "not_submitted"
@@ -45,31 +41,31 @@ class SubmitScoreStatus(str, Enum):
 
 class TestSample(BaseModel):
     input: MdCodeContent = Field(..., description="输入")
-    realOutput: MdCodeContent = Field(..., description="真实输出")
-    expectOutput: MdCodeContent = Field(..., description="期望输出")
+    real_output: MdCodeContent = Field(..., description="真实输出")
+    expect_output: MdCodeContent = Field(..., description="期望输出")
 
 class Submit(BaseModel):
     score: float = Field(..., description="提交分数")
     time: datetime = Field(..., description="提交时间")
     #! py3.9 以上直接支持 list[] 语法而不用导入 List
-    testSample: list[TestSample] = Field(..., description="测试样例")
-    submitCode: list[CodeFileInfo] = Field(..., description="提交代码文件列表")
+    test_sample: list[TestSample] = Field(..., description="测试样例")
+    submit_code: list[CodeFileInfo] = Field(..., description="提交代码文件列表")
 
 class AssignData(BaseModel):
-    assignId: AssignId = Field(..., description="作业ID")
+    assign_id: AssignId = Field(..., description="作业ID")
     title: str = Field(..., description="作业标题")
     description: str = Field(..., description="作业描述")
-    assignOriginalCode: list[CodeSet] = Field(..., description="作业原始代码")
+    assign_original_code: list[CodeFileInfo] = Field(..., description="作业原始代码")
     submit: Optional[Submit] = Field(..., description="作业提交记录")
 
 class BasicAnalysis(BaseModel):
     resolution:Optional[MatrixAnalysisProps] = Field(None, description="题目解答")
-    knowledgeAnalysis: Optional[MatrixAnalysisProps] = Field(None, description="知识点分析")
+    knowledge_analysis: Optional[MatrixAnalysisProps] = Field(None, description="知识点分析")
 
 class AiGenAnalysis(BaseModel):
-    codeAnalysis:Optional[ MatrixAnalysisProps] = Field(None, description="当前代码分析")
-    learningSuggestions: Optional[MatrixAnalysisProps] = Field(None, description="学习建议")
+    code_analysis:Optional[ MatrixAnalysisProps] = Field(None, description="当前代码分析")
+    learning_suggestions: Optional[MatrixAnalysisProps] = Field(None, description="学习建议")
 
 class Analysis(BaseModel):
     basic: BasicAnalysis = Field(..., description="基础分析")
-    aiGen: Optional[AiGenAnalysis] = Field(None, description="AI生成分析")
+    ai_gen: Optional[AiGenAnalysis] = Field(None, description="AI生成分析")
--- backend/app/schemas/course.py
+++ backend/app/schemas/course.py
@@ -7,36 +7,32 @@ from datetime import datetime
 from assignment import AssignData, TestSample, CodeSet
 from general import AssignId
 from enum import Enum
-
-class CourseBaseModel(BaseModel):
-    id: int = Field(..., description="课程ID")
-    name: str = Field(..., description="课程名称")
-    assignment: list[AssignData] = Field(..., description="课程作业列表")
-    completed: bool = Field(..., description="是否完成")
-
-class CourseResponseModel(CourseBaseModel):
-    completed: bool = Field(..., description="是否已完成课程")
-
-class SubmissionModel(BaseModel):
-    score: float = Field(..., description="提交分数")
-    time: datetime = Field(..., description="提交时间")
-    sample: TestSample = Field(..., description="样例输入输出")
-    assignCode: list[CodeSet] = Field(..., description="作业代码")
-
-
-class AssignmentResponseModel(AssignData):
-    description: str = Field(..., description="作业描述")
-    assignOriginalCode: list[CodeSet] = Field(..., description="作业原始代码")
-    submit: SubmissionModel = Field(..., description="作业提交信息")
+from general import CourseId
 
 class AssignType(str, Enum):
     CHOOSE = "choose"
     PROGRAM = "program"
 
 class AssignmentListItem(BaseModel):
-    assignId: AssignId
-    assignmentName: str
+    assign_id: AssignId
+    assignment_name: str
     type: AssignType
     score: Optional[float]
     ddl: Optional[datetime]
 
+class CourseBase(BaseModel):
+    id: CourseId = Field(..., description="课程ID")
+    course_name: str = Field(..., description="课程名称")
+
+class Course(CourseBase):
+    assignment: Optional[list[AssignmentListItem]] = Field(..., description="课程作业列表")
+    completed: bool = Field(..., description="是否完成")
+
+class TodoCourse(CourseBase):
+    assignment: list[AssignmentListItem] = Field(..., description="课程作业列表")
+
+class SubmissionModel(BaseModel):
+    score: float = Field(..., description="提交分数")
+    time: datetime = Field(..., description="提交时间")
+    sample: TestSample = Field(..., description="样例输入输出")
+    assignCode: list[CodeSet] = Field(..., description="作业代码")
