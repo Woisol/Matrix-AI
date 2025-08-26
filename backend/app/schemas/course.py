78768d9bd9fe94b6c17af2412c8bc636f6af8617
Woisol-G <Woisol-G@outlook.com>
2025-08-26 18:01:15 +0800

    feat[backend]: implement course schemas

--- backend/app/schemas/course.py
+++ backend/app/schemas/course.py
@@ -2,16 +2,19 @@
 课程相关的Pydantic Schema
 """
 from pydantic import BaseModel, Field
-# from typing import Optional, List, Any, Dict
+from typing import Optional
 from datetime import datetime
+from assignment import AssignData, TestSample, CodeSet
+from general import AssignId
+from enum import Enum
 
 class CourseBaseModel(BaseModel):
     id: int = Field(..., description="课程ID")
     name: str = Field(..., description="课程名称")
-    description: str = Field(..., description="课程描述")
+    assignment: list[AssignData] = Field(..., description="课程作业列表")
+    completed: bool = Field(..., description="是否完成")
 
 class CourseResponseModel(CourseBaseModel):
-    assignment: list[AssignmentModel] = Field(..., description="课程作业列表")
     completed: bool = Field(..., description="是否已完成课程")
 
 class SubmissionModel(BaseModel):
@@ -21,10 +24,19 @@ class SubmissionModel(BaseModel):
     assignCode: list[CodeSet] = Field(..., description="作业代码")
 
 
-class AssignmentResponseModel(AssignmentModel):
+class AssignmentResponseModel(AssignData):
     description: str = Field(..., description="作业描述")
     assignOriginalCode: list[CodeSet] = Field(..., description="作业原始代码")
     submit: SubmissionModel = Field(..., description="作业提交信息")
 
+class AssignType(str, Enum):
+    CHOOSE = "choose"
+    PROGRAM = "program"
 
+class AssignmentListItem(BaseModel):
+    assignId: AssignId
+    assignmentName: str
+    type: AssignType
+    score: Optional[float]
+    ddl: Optional[datetime]
 
