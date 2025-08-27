2ebe6bd14d1a1add91d67117e7d82c45c157580e
Woisol-G <Woisol-G@outlook.com>
2025-08-27 17:45:31 +0800

    fix, feat[backend]: WIP try fixing codes table struct and test run request failure, implementing submit logic

--- backend/app/controller/assignment.py
+++ backend/app/controller/assignment.py
@@ -1,4 +1,4 @@
-import uuid
+import uuid, json
 from datetime import datetime
 from typing import Optional
 
@@ -8,13 +8,14 @@ from tortoise import exceptions as torExceptions
 from app.models.course import Course as CourseModel
 from app.models.assignment import Assignment as AssignmentModel
 from app.models.playground import Playground
-from app.schemas.assignment import AssignData, Submit, SubmitRequest, TestSample, CodeFileInfo
+from app.schemas.general import CourseId, AssignId
+from app.schemas.assignment import AssignData, Submit, TestSubmitRequest,SubmitRequest, TestSample, TestSampleCreate, CodeFileInfo
 
 from app.utils.assign import listStrToList
 
 
 class AssignmentController:
-    async def get_assignment(assign_id: str = Path(..., description="作业ID")) -> AssignData:
+    async def get_assignment(assign_id: str) -> AssignData:
         try:
             assignment = await AssignmentModel.get(id=assign_id).prefetch_related("codes").prefetch_related("submissions")
             if not assignment:
@@ -47,11 +48,13 @@ class AssignmentController:
             raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
 
     async def create_assignment(
-        course_id: str = Path(..., description="课程ID"),
-        title: str = Form(..., description="作业标题"),
-        description: str = Form(..., description="作业描述"),
-        assignOriginalCode: str = Form(..., description="作业原始代码"),
-        ddl: Optional[str] = Form(None, description="作业截止日期"),
+        course_id: str,
+        title: str,
+        description: str,
+        assignOriginalCode: str,
+        testSample: TestSampleCreate,
+        # testSample: TestSampleCreate,
+        ddl: Optional[str],
     ) -> AssignData:
         try:
             course = await CourseModel.get(id=course_id)
@@ -61,7 +64,16 @@ class AssignmentController:
                 description=description,
                 type="program",
                 end_date=ddl if ddl else None,
+                # original_code=assignOriginalCode,
+            )
+            # _input_str = json.dumps(testSample.input, ensure_ascii=False)
+            await assignment.codes.instance.create(
+                id=uuid.uuid4().hex,
                 original_code=assignOriginalCode,
+                sample_input='',
+                sample_expect_output='',
+                # sample_input=json.dumps(testSample.input, ensure_ascii=False),
+                # sample_expect_output=json.dumps(testSample.expectOutput, ensure_ascii=False),
             )
             await course.assignments.add(assignment)
             return AssignData(
@@ -76,7 +88,7 @@ class AssignmentController:
             raise HTTPException(status_code=400, detail="Invalid data provided")
         except Exception as e:
             raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
-    async def submit_code(submitRequest: SubmitRequest) -> str:
+    async def test_submit(submitRequest: TestSubmitRequest) -> str:
         try:
             # 处理提交逻辑
             output = await Playground.run_code(
@@ -85,5 +97,15 @@ class AssignmentController:
                 language=submitRequest.language,
             )
             return output
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
+    async def submit_code(course_id:CourseId,assign_id:AssignId, submitRequest:SubmitRequest ):
+        try:
+            # 处理提交逻辑
+            output = await Playground.run_code(
+                code=submitRequest.codeFile.content,
+                input="",
+                language="c_cpp",
+            )
         except Exception as e:
             raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
