3201af0902c9e175f1bbcef0f6416b85a91694f6
Woisol-G <Woisol-G@outlook.com>
2025-08-28 18:13:13 +0800

    feat[backend]: implement resolution generate

--- backend/app/controller/ai.py
+++ backend/app/controller/ai.py
@@ -11,7 +11,7 @@ from app.models.assignment import Assignment as AssignmentModel
 from app.models.ai import AI
 from app.schemas.course import Course as CourseData
 from app.schemas.general import CourseId, AssignId
-from app.schemas.assignment import AssignData, Submit, TestSubmitRequest,SubmitRequest, TestSample, TestSampleCreate, CodeFileInfo, MatrixAnalysisProps
+from app.schemas.assignment import AssignData, Submit, TestSubmitRequest,SubmitRequest, TestSample, TestSampleCreate, CodeFileInfo, MatrixAnalysisProps, MatrixAnalysisContent, BasicAnalysis
 from app.schemas.ai import AiResponse
 from app.constants.prompt import AIPrompt
 from assignment import AssignmentController
@@ -27,15 +27,33 @@ from openai import OpenAI
 class TestAiController:
     """用于控制测试相关的AI服务的控制器.由于申请api时间有限，现使用通义千问的统一模型接口进行测试"""
     @classmethod
-    async def genResolutions(cls, course_id: str, assign_id: str):
+    async def genResolutions(cls, course_id: str, assign_id: str) -> MatrixAnalysisProps:
         try:
             assign_data: AssignData = await AssignmentController.get_assignment(assign_id)
             # course_data: CourseData = await CourseController.get_course(course_id)
 
-            content = AI.getResponse(prompt=AIPrompt.RESOLUTION(
+            # 获取所有可能解法
+            _resoltContent = await AI.getResponse(prompt=AIPrompt.RESOLUTION(
                 assign_data.title, assign_data.description, assign_data.assignOriginalCode[0].content))
 
-            return BasicAnalysis.resolution(**content)
+            resoltContents = [c.strip() for c in _resoltContent.split("\n") if c.strip() != ""]
+
+            # 生成标题
+            resoltTitle = [await AI.getResponse(AIPrompt.TITLE_CODE(code)) for code in resoltContents]
+
+            analysis = MatrixAnalysisProps(
+                content=[
+                    MatrixAnalysisContent(
+                        title=t,
+                        content=c
+                    ) for t, c in zip(resoltTitle, resoltContents)
+                ],
+                #@todo implement summary prompt and logic
+                summary=""
+                showInEditor=False
+            )
+
+            return analysis
 
         except Exception as e:
             raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
