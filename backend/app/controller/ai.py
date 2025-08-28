4bba5d0cdc3e5d2df7410f2848bb06adce39f2f2
Woisol-G <Woisol-G@outlook.com>
2025-08-28 18:33:56 +0800

    refactor[backend]: refactor ai analysis generator, add analysis table, implement basic and aiGen route

--- backend/app/controller/ai.py
+++ backend/app/controller/ai.py
@@ -8,15 +8,14 @@ from tortoise import exceptions as torExceptions
 
 from app.models.course import Course as CourseModel
 from app.models.assignment import Assignment as AssignmentModel
-from app.models.ai import AI
+from app.models.ai import AI, AIAnalysisGenerator
 from app.schemas.course import Course as CourseData
 from app.schemas.general import CourseId, AssignId
-from app.schemas.assignment import AssignData, Submit, TestSubmitRequest,SubmitRequest, TestSample, TestSampleCreate, CodeFileInfo, MatrixAnalysisProps, MatrixAnalysisContent, BasicAnalysis
+from app.schemas.assignment import AssignData, Submit, TestSubmitRequest,SubmitRequest, TestSample, TestSampleCreate, CodeFileInfo, MatrixAnalysisProps, MatrixAnalysisContent, BasicAnalysis, AiGenAnalysis
 from app.schemas.ai import AiResponse
 from app.constants.prompt import AIPrompt
 from assignment import AssignmentController
 from course import CourseController
-from app.utils.assign import listStrToList
 
 import os
 from openai import OpenAI
@@ -24,37 +23,39 @@ from openai import OpenAI
 # url = "http://10.10.1.11:38666/v1/chat/completions"
 #模型使用url+request库进行调用，需要把格式包入message体中
 
-class TestAiController:
+class AIController:
     """用于控制测试相关的AI服务的控制器.由于申请api时间有限，现使用通义千问的统一模型接口进行测试"""
     @classmethod
-    async def genResolutions(cls, course_id: str, assign_id: str) -> MatrixAnalysisProps:
-        try:
-            assign_data: AssignData = await AssignmentController.get_assignment(assign_id)
-            # course_data: CourseData = await CourseController.get_course(course_id)
-
-            # 获取所有可能解法
-            _resoltContent = await AI.getResponse(prompt=AIPrompt.RESOLUTION(
-                assign_data.title, assign_data.description, assign_data.assignOriginalCode[0].content))
-
-            resoltContents = [c.strip() for c in _resoltContent.split("\n") if c.strip() != ""]
-
-            # 生成标题
-            resoltTitle = [await AI.getResponse(AIPrompt.TITLE_CODE(code)) for code in resoltContents]
-
-            analysis = MatrixAnalysisProps(
-                content=[
-                    MatrixAnalysisContent(
-                        title=t,
-                        content=c
-                    ) for t, c in zip(resoltTitle, resoltContents)
-                ],
-                #@todo implement summary prompt and logic
-                summary=""
-                showInEditor=False
+    async def getBasic(cls, course_id: str, assign_id: str) -> BasicAnalysis:
+        assignment = await AssignmentModel.get(id=assign_id).prefetch_related("analysis")
+        analysis = assignment.analysis[0] if assignment.analysis else None
+        if analysis:
+            return BasicAnalysis(
+                resolution=json.loads(analysis.resolution),
+                knowledgeAnalysis=json.loads(analysis.knowledge_analysis)
+            )
+        else:
+            #@todo add to queue instead
+            resol = await AIAnalysisGenerator.genResolutions(course_id, assign_id)
+            knowled = await AIAnalysisGenerator.genKnowledgeAnalysis(course_id, assign_id)
+            return BasicAnalysis(
+                resolution=resol,
+                knowledgeAnalysis=knowled
             )
 
-            return analysis
-
-        except Exception as e:
-            raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
-
+    async def getAiGen(cls, course_id: str, assign_id: str) -> AiGenAnalysis:
+        assignment = await AssignmentModel.get(id=assign_id).prefetch_related("analysis")
+        analysis = assignment.analysis[0] if assignment.analysis else None
+        if analysis:
+            return AiGenAnalysis(
+                codeAnalysis=json.loads(analysis.code_analysis),
+                learningSuggestions=json.loads(analysis.learning_suggestions)
+            )
+        else:
+            #@todo add to queue instead
+            codeAnal = await AIAnalysisGenerator.genCodeAnalysis(course_id, assign_id)
+            learnSug = await AIAnalysisGenerator.genLearningSuggestions(course_id, assign_id)
+            return await AiGenAnalysis(
+                codeAnalysis=codeAnal,
+                learningSuggestions=learnSug
+            )
