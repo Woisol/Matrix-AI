e2c5c49b63ac8072bcf2fa71bed34631a50623e7
Woisol-G <Woisol-G@outlook.com>
2025-08-30 19:24:25 +0800

    feat, fix: implement re-gen and more bug fixing
    backend:
    1. implement basic re-gen
    2. implement multi content tab for generation
    3. fix bad sample format crashing-leading,
    4. fix bad table store format
    
    frontend:
    1. add basic re-gen button
    2. add generation toast
    3 remove default ai-gen content fetching

--- backend/app/controller/ai.py
+++ backend/app/controller/ai.py
@@ -13,31 +13,54 @@ from datetime import datetime, timezone
 class AIController:
     """用于控制测试相关的AI服务的控制器.由于申请api时间有限，现使用通义千问的统一模型接口进行测试"""
     @classmethod
-    async def getBasic(cls, course_id: str, assign_id: str) -> BasicAnalysis:
+    async def getBasic(cls, course_id: str, assign_id: str, re_gen: bool = False) -> BasicAnalysis:
         #@todo 可能需要重新生成功能 /basic/again ？
         assignment = await AssignmentModel.get(id=assign_id).prefetch_related("analysis")
         analysis = assignment.analysis[0] if assignment.analysis else None
-        if analysis:
+
+        if re_gen:
+            resol = await AIAnalysisGenerator.genResolutions(course_id, assign_id)
+            knowled = await AIAnalysisGenerator.genKnowledgeAnalysis(course_id, assign_id)
+            if analysis:
+                analysis.resolution = resol.model_dump_json()
+                analysis.knowledge_analysis = knowled.model_dump_json()
+                await analysis.save()
+            else:
+                #@todo add to queue instead
+                analysis = await Analysis.create(
+                    assignment=assignment,
+                    resolution=resol.model_dump_json(),
+                    knowledge_analysis=knowled.model_dump_json()
+                )
+                await analysis.save()
+            return BasicAnalysis(
+                resolution=resol,
+                knowledgeAnalysis=knowled
+            )
+        elif analysis:
             return BasicAnalysis(
                 resolution=analysis.resolution,
                 knowledgeAnalysis=analysis.knowledge_analysis
             )
         else:
-            #@todo add to queue instead
             resol = await AIAnalysisGenerator.genResolutions(course_id, assign_id)
             knowled = await AIAnalysisGenerator.genKnowledgeAnalysis(course_id, assign_id)
+            #@todo add to queue instead
             analysis = await Analysis.create(
                 assignment=assignment,
-                resolution=json.dumps(resol.model_dump_json()),
-                knowledge_analysis=json.dumps(knowled.model_dump_json())
+                resolution=resol.model_dump_json(),
+                knowledge_analysis=knowled.model_dump_json()
             )
             await analysis.save()
             return BasicAnalysis(
                 resolution=resol,
                 knowledgeAnalysis=knowled
             )
+
+
+
     @classmethod
-    async def getAiGen(cls, course_id: str, assign_id: str) -> AiGenAnalysis:
+    async def getAiGen(cls, course_id: str, assign_id: str, re_gen: bool = False) -> AiGenAnalysis:
         assignment = await AssignmentModel.get(id=assign_id).prefetch_related("analysis","submissions")
         if assignment.end_date and assignment.end_date > datetime.now(timezone.utc):
             raise HTTPException(status_code=400, detail="Deadline not meet yet")
@@ -47,15 +70,26 @@ class AIController:
             raise HTTPException(status_code=403, detail="AI生成分析功能需要在提交作业后才能使用哦~")
 
         analysis = assignment.analysis[0] if assignment.analysis else None
-        if analysis:
+        if analysis and analysis.code_analysis and analysis.learning_suggestions:
             return AiGenAnalysis(
-                codeAnalysis=json.loads(analysis.code_analysis),
-                learningSuggestions=json.loads(analysis.learning_suggestions)
+                codeAnalysis=analysis.code_analysis,
+                learningSuggestions=analysis.learning_suggestions
             )
         else:
             #@todo add to queue instead
             codeAnal = await AIAnalysisGenerator.genCodeAnalysis(course_id, assign_id)
             learnSug = await AIAnalysisGenerator.genLearningSuggestions(course_id, assign_id)
+            if analysis:
+                analysis.code_analysis = codeAnal.model_dump_json()
+                analysis.learning_suggestions = learnSug.model_dump_json()
+                await analysis.save()
+            else:
+                analysis = await Analysis.create(
+                    assignment=assignment,
+                    code_analysis=codeAnal.model_dump_json(),
+                    learning_suggestions=learnSug.model_dump_json()
+                )
+                await analysis.save()
             return AiGenAnalysis(
                 codeAnalysis=codeAnal,
                 learningSuggestions=learnSug
--- backend/app/routers/ai.py
+++ backend/app/routers/ai.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from app.controller.ai import AIController
 from app.schemas import ai as ai_schema
 
@@ -9,8 +9,9 @@ ai_route = APIRouter(tags=["ai"])
 async def basic_analysis(
     course_id: str,
     assign_id: str,
+    reGen: bool = Query(False, description="是否重新生成分析")
 ):
-    return await AIController.getBasic(course_id, assign_id)
+    return await AIController.getBasic(course_id, assign_id, re_gen=reGen)
 
 @ai_route.get("/courses/{course_id}/assignments/{assign_id}/analysis/aiGen")
 async def generate_analysis(
